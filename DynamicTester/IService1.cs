b58e7d2b381bac4c6213817a5425f8404388b50b
Natalie Moes <107507852+NatalieMoes@users.noreply.github.com>
2022-06-22 13:56:06 -0700

    Add files via upload

---
+++ DynamicTester/IService1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace DynamicTesting
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        /// <summary>
+        /// Tests an operation of a service passing the argument input as the
+        /// input to the operation call. As this only tests one operation, the other
+        /// operations in ServiceResults.OpInfo will have null Input and output values.
+        /// This operation will be able to modify one operation's results from the same 
+        /// ServiceResults instance, and will test the same operation again if the input 
+        /// argument differs from the instance passed to it in a preceding call.  
+        /// This operation can also be invoked before invoking TestServices to test the
+        /// services' operations that take contextually-sensitive or non-value-type input to
+        /// generate valid output for all operations for those services.
+        /// </summary>
+        /// <param name="url">The URL to the WSDL page of the service to test.</param>
+        /// <param name="opName">The name of the operation to test.</param>
+        /// <param name="input">The argument values for the operation being tested.</param>
+        /// <returns></returns>
+        [OperationContract]
+        ServiceResults TestOperationGivenInput(string url, string opName, object[] input);
+
+
+        /// <summary>
+        /// Tests a number of services to verify whether they can be accessed and 
+        /// generates input that will be of the correct type for value-type parameters,
+        /// but may not be contextually appropriate for those parameters.  Thus, this
+        /// operation will potentially get successful output for operations
+        /// offered by a service, but is not garaunteed to do so.  This operation also
+        /// caches services that were successfully bound to and shares this cache with 
+        /// TestOperationGivenInput, so it only tests the operations of a service that
+        /// have either not been tested or were tested with different input but were 
+        /// unsuccessful.
+        /// </summary>
+        /// <param name="urls">An array containing the urls of the WSDL pages for the services to test.</param>
+        /// <returns></returns>
+        [OperationContract]
+        Dictionary<string, ServiceResults> TestServices(string[] urls);
+
+
+        /// <summary>
+        /// Invokes a given operation from the RESTful service at the given parameter names and corresponding 
+        /// input values returns the operation's output.
+        /// </summary>
+        /// <param name="baseUrl">The base url of the RESTful service to test.</param>
+        /// <param name="opName">The name of the operation of the RESTful service to test.</param>
+        /// <param name="paramName">An array containing the parameter names for the operation to test.</param>
+        /// <param name="input">An array containing the argument values for the operation to test.</param>
+        /// <exception cref="ArgumentException(string)"></exception>
+        /// <exception cref="ArgumentNullException(string)"></exception>
+        /// <returns></returns>    
+        [OperationContract]
+        string TestRESTfulService(string baseUrl, string opName, string[] paramName, string[] input);
+    }
+
+
+    /// <summary>
+    /// Contains information related to the testing of a WSDL
+    /// service and its operations.
+    /// </summary>
+    [DataContract]
+    public class ServiceResults
+    {
+        private string _url;
+        private string _name = "";
+        private bool _result = false;
+        private bool _fromCache = false;
+        private Dictionary<string, OpInfoInput> _opInfo = new Dictionary<string, OpInfoInput>();
+        private List<string> _exceptionMessages = new List<string>();
+        private static Dictionary<string, ServiceResults> _cache = new Dictionary<string, ServiceResults>();
+        
+        
+        /// <summary>
+        /// The default Constructor for the ServiceResults class.
+        /// </summary>
+        public ServiceResults() { }
+
+
+        /// <summary>
+        /// Initializes an instance of the ServiceResults class with
+        /// given URL value.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <exception cref="ArgumentNullException(string)"></exception>
+        public ServiceResults(string url)
+        {
+            _url = url ?? throw new ArgumentNullException(nameof(url));
+        }
+
+
+        /// <summary>
+        /// Loads _opInfo with OpInfoInput objects that have been initialized from
+        /// the given OperationInfoCollection instance
+        /// </summary>
+        /// <param name="operationInfos"></param>
+        public void InitializeFrom(Dictionary<string, WsOperations.OperationInfo> operationInfos)
+        {
+            foreach (KeyValuePair<string, WsOperations.OperationInfo> info in operationInfos)
+            {
+                _opInfo.Add(info.Key, new OpInfoInput(info.Value));
+            }
+        }
+
+
+        /// <summary>
+        /// The URL of this service's WSDL page.
+        /// </summary>
+        [DataMember]
+        public string Url { get => _url; set => _url = value; }
+
+        /// <summary>
+        /// Indicates if this service was available at the time it was tested.
+        /// </summary>
+        [DataMember]
+        public bool Result { get => _result; set => _result = value; }
+
+        /// <summary>
+        /// The name of the service.
+        /// </summary>
+        [DataMember]
+        public string Name { get => _name; set => _name = value; }
+
+        /// <summary>
+        /// A list of the messages of exceptions that occurred during testing.
+        /// </summary>
+        [DataMember]
+        public List<string> ExceptionMessages { get => _exceptionMessages; set => _exceptionMessages = value; }
+
+        /// <summary>
+        /// The cache shared by all instances of ServiceResults, storing ServiceResults instances keyed
+        /// by URL.
+        /// </summary>
+        [DataMember]
+        public static Dictionary<string, ServiceResults> Cache { get => _cache; set => _cache = value; }
+
+        /// <summary>
+        /// A dictionary containing OpInfoInput instances keyed by operation name,
+        /// which store information about the service's operations, including the 
+        /// input and output values obtained when the operation was tested and 
+        /// whether the operation was executed successfully.
+        /// </summary>
+        [DataMember]
+        public Dictionary<string, OpInfoInput> OpInfo { get => _opInfo; set => _opInfo = value; }
+
+        /// <summary>
+        /// Indicates whether TestOperationGivenInput or TestServices obtained
+        /// this ServiceResults instance while it was in the cache. 
+        /// </summary>
+        [DataMember]
+        public bool FromCache { get => _fromCache; set => _fromCache = value; }
+    }
+
+
+    /// <summary>
+    /// Extends WsOperations.OperationInfo to include information relevant 
+    /// to testing an operation, such as the input and output values gathered
+    /// and whether the operation executed successfully during testing.
+    /// </summary>
+    [DataContract]
+    public class OpInfoInput : WsOperations.OperationInfo
+    {
+        private List<object> _input = null;
+        private object _output;
+        private bool _isValid = false;
+
+
+        /// <summary>
+        /// Initializes an OpInfoInput object from an OperationInfo instance
+        /// to instantiate members inherited from OperationInfo 
+        /// </summary>
+        /// <param name="opInfo"></param>
+        public OpInfoInput(WsOperations.OperationInfo opInfo)
+        {
+            Name = opInfo.Name;
+            PortTypeName = opInfo.PortTypeName;
+            InputParameters = opInfo.InputParameters;
+            OutputParameters = opInfo.OutputParameters;
+        }
+ 
+
+        /// <summary>
+        /// Determines if another List<object> instance is identical to the
+        /// member _input of this OpInfoInput instance.
+        /// </summary>
+        /// <param name="list2"></param>
+        /// <returns></returns>
+        public bool InputEquals(List<object> list2)
+        {
+            if (list2 == null || _input == null) return false;
+
+            var firstNotSecond = _input.Except(list2).ToList();
+            var secondNotFirst = list2.Except(_input).ToList();
+            return !firstNotSecond.Any() && !secondNotFirst.Any();
+        }
+
+
+        /// <summary>
+        /// A list of objects storing the argument values corresponding to
+        /// the parameters of the operation.
+        /// </summary>
+        [DataMember]
+        public List<object> Input { get => _input; set => _input = value; }
+
+        /// <summary>
+        /// The output returned by the operation, which will be null if the 
+        /// operation did not execute or was not invoked successfully.
+        /// </summary>
+        [DataMember]
+        public object Output { get => _output; set => _output = value; }
+
+        /// <summary>
+        /// Indicates whether the operation executed and/or was invoked successfully.
+        /// </summary>
+        [DataMember]
+        public bool IsValid { get => _isValid; set => _isValid = value; }     
+    }   
+
+}
---
+++ DynamicTester/Service1.svc.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using WcfSamples.DynamicProxy;
+
+namespace DynamicTesting
+{
+    public class Service1 : IService1
+    {
+        public ServiceResults TestOperationGivenInput(string url, string opName, object[] input)
+        {
+            ServiceResults serviceResults = null;
+
+            // Determine if this instance of ServiceResults is already in cache
+            if (ServiceResults.Cache.TryGetValue(url, out ServiceResults s) && s != null
+                && s.OpInfo.TryGetValue(opName, out OpInfoInput op) && op.InputEquals(new List<object>(input))) // is in cache -- url, operation name, and input are all the same 
+            {
+                serviceResults = s;
+                serviceResults.FromCache = true;
+            }
+            else   // not in the cache
+            {   // make proxy, call method, store results in cache, and return them              
+                serviceResults = new ServiceResults(url);
+                try
+                {
+                    WsOperations.Service1Client client = new WsOperations.Service1Client();
+                    WsOperations.WebServiceInfo info = client.GetWebServiceInfo(url); // get service information
+
+                    if (info.Ops.ContainsKey(opName)) // service has the operation
+                    {   // store relevent information from WebServiceInfo                    
+                        serviceResults.InitializeFrom(info.Ops);
+                        serviceResults.Name = info.ServiceName;
+                        serviceResults.OpInfo[opName].Input = new List<object>(input);
+
+                        DynamicProxyFactory factory = new DynamicProxyFactory(serviceResults.Url);
+                        serviceResults.Result = true;
+                        TestOperation(factory, serviceResults, opName);
+                    }
+                    else // service does not provide the given operation
+                    {
+                        serviceResults.ExceptionMessages.Add("Operation could not be found for service.");
+                    }
+                }
+                catch (Exception exc)  // failed to get operation info
+                {
+                    serviceResults.Result = false;
+                    serviceResults.ExceptionMessages.Add(exc.Message);
+                }
+            }
+
+            if (serviceResults != null && serviceResults.Result && serviceResults.OpInfo[opName].IsValid) // add result to cache if operation call was successful
+            {
+                if (ServiceResults.Cache.ContainsKey(url))
+                {
+                    ServiceResults.Cache[url] = serviceResults;
+                }
+                else
+                {
+                    ServiceResults.Cache.Add(url, serviceResults);
+                }
+            }
+
+            return serviceResults;
+        }
+
+
+        public Dictionary<string, ServiceResults> TestServices(string[] urls)
+        {
+            WsOperations.Service1Client client = new WsOperations.Service1Client();
+            Dictionary<string, ServiceResults> dict = new Dictionary<string, ServiceResults>();
+            DynamicProxyFactory factory;
+            WsOperations.WebServiceInfo info;
+
+            foreach (string wsdl in urls)
+            {
+                if (ServiceResults.Cache.TryGetValue(wsdl, out ServiceResults s) && s != null)  // if in cache 
+                {
+                    s.FromCache = true;
+                    try
+                    {
+                        factory = new DynamicProxyFactory(wsdl);
+
+                        foreach (KeyValuePair<string, OpInfoInput> opIn in s.OpInfo) // check/test each operation
+                        {
+                            //  do not test if it was successfully tested or if it failed w/ generated input 
+                            if (!opIn.Value.IsValid)  // was either not tested or tested w/ bad input
+                            {
+                                List<object> input = GenerateInput(opIn.Value);
+                                if (!opIn.Value.InputEquals(input)) // not tested or tested w/ bad non-generated input
+                                {
+                                    opIn.Value.Input = input;
+                                    TestOperation(factory, s, opIn.Key);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception exc)  // DynamicProxyFactory creation failed 
+                    {
+                        s.Result = false;
+                        s.ExceptionMessages.Add(exc.Message);
+                    }
+                    finally
+                    {
+                        try // add to returned dictionary
+                        {
+                            dict.Add(wsdl, s);
+                        }
+                        catch (ArgumentException) { }
+                    }
+                }
+
+                else   // not in cache
+                {
+                    ServiceResults serviceResults = new ServiceResults(wsdl);
+                    try
+                    {
+                        factory = new DynamicProxyFactory(wsdl);
+                        info = client.GetWebServiceInfo(wsdl);   // instantiate webmethodinfo using WsOperations
+                        serviceResults.Result = true;
+                        serviceResults.Name = info.ServiceName;
+                        serviceResults.InitializeFrom(info.Ops);
+
+                        foreach (KeyValuePair<string, OpInfoInput> opIn in serviceResults.OpInfo) // for each operation
+                        {
+                            opIn.Value.Input = GenerateInput(opIn.Value);
+                            TestOperation(factory, serviceResults, opIn.Key);
+                        }
+                    }
+                    catch (Exception exc)  // proxy instantiation failed
+                    {
+                        serviceResults.Result = false;
+                        serviceResults.ExceptionMessages.Add(exc.Message);
+                    }
+                    finally
+                    {
+                        try // add ServiceResults to returned dictionary and cache
+                        {
+                            dict.Add(wsdl, serviceResults);
+                            // add to cache
+                            if (serviceResults.Result)
+                            {
+                                ServiceResults.Cache.Add(wsdl, serviceResults);
+                            }
+                        }
+                        catch (ArgumentException) { }
+                    }
+                }
+            }
+            return dict;
+        }
+
+
+        public string TestRESTfulService(string baseUrl, string opName, string[] paramName, string[] input)
+        {
+            //   Validate argument values 
+            if (string.IsNullOrEmpty(baseUrl))
+            {
+                throw new ArgumentException($"'{nameof(baseUrl)}' cannot be null or empty.", nameof(baseUrl));
+            }
+
+            if (string.IsNullOrEmpty(opName))
+            {
+                throw new ArgumentException($"'{nameof(opName)}' cannot be null or empty.", nameof(opName));
+            }
+
+            //  construct complete url
+            string completeUrl = baseUrl + "/" + opName;
+
+            if (paramName != null && input != null && paramName.Length > 0 && input.Length > 0) // operation has params. & args.
+            {
+                if (paramName.Length != input.Length)   // check argument validity
+                {
+                    throw new ArgumentException($"'{nameof(paramName)} and '{nameof(input)}' must have the same length.");
+                }
+
+                // format parameters and input to form complete url             
+                string[] templateStr = new string[paramName.Length];
+                for (int i = 0; i < paramName.Length; i++)
+                {
+                    templateStr[i] = string.Format("{0}={1}", paramName[i], input[i]);
+                }
+                completeUrl += "?" + string.Join("&", templateStr);  // add formatted parameters and arguments
+            }
+
+            // use Web2String service to download the service's output and return it
+            Web2String.ServiceClient client = new Web2String.ServiceClient();
+            return client.GetWebContent(completeUrl);
+        }
+
+
+
+        /// <summary>
+        /// Instantiates a DynamicProxy from the DynamicProxyFactory instance factory and
+        /// calls the operation given by opName using the input instance for the operation
+        /// and stores the output as well as whether the operation successfully executed 
+        /// in the operation's corresponding OpInfoInput instance located in serviceResults.OpInfo.
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <param name="serviceResults"></param>
+        /// <param name="opName"></param>
+        private void TestOperation(DynamicProxyFactory factory, ServiceResults serviceResults, string opName)
+        {
+            try
+            {
+                DynamicProxy proxy = factory.CreateProxy(serviceResults.OpInfo[opName].PortTypeName);
+
+                // store output from operation invocation
+                try
+                {
+                    object output = proxy.CallMethod(opName, serviceResults.OpInfo[opName].Input.ToArray());
+                    if (output != null)
+                    {
+                        serviceResults.OpInfo[opName].Output = output.ToString();
+                    }
+                    else
+                    {
+                        serviceResults.OpInfo[opName].Output = "null";
+                    }
+
+                    serviceResults.OpInfo[opName].IsValid = true;  // operation successfully returned
+                }
+                catch (Exception exc)  // operation invocation likely failed
+                {
+                    serviceResults.ExceptionMessages.Add(exc.Message);
+                }
+            }
+            catch (Exception exc)  // creation of dynamic proxy likely failed
+            {
+                serviceResults.Result = false;
+                serviceResults.ExceptionMessages.Add(exc.Message);
+            }
+        }
+
+        /// <summary>
+        /// Generates sample argument values for the input parameter values
+        /// of the operation represented by opInfo.
+        /// NOTE: for non-value types and other types that have not been considered, 
+        /// the null value is returned.
+        /// </summary>
+        /// <param name="opInfo"></param>
+        /// <returns></returns>
+        private List<object> GenerateInput(OpInfoInput opInfo)
+        {
+            List<object> input = new List<object>();
+            // extract type(s) from input parameter(s) and generate input value(s)
+            foreach (WsOperations.Parameter parameter in opInfo.InputParameters)
+            {
+                switch (parameter.Type)     // generate certain input based on type
+                {
+                    case "string":
+                        input.Add("Test Input");
+                        break;
+                    case "char":
+                        input.Add('a');
+                        break;
+                    case "int":
+                        input.Add(50);
+                        break;
+                    case "long":
+                        input.Add(50);
+                        break;
+                    case "unsignedlong":
+                        input.Add(50);
+                        break;
+                    case "short":
+                        input.Add(50);
+                        break;
+                    case "unsignedshort":
+                        input.Add(50);
+                        break;
+                    case "float":
+                        input.Add(50.5);
+                        break;
+                    case "double":
+                        input.Add(50.5);
+                        break;
+                    case "decimal":
+                        input.Add(50.5);
+                        break;
+                    case "boolean":
+                        input.Add(false);
+                        break;
+                    default:     // most likely an object 
+                        input.Add(null);
+                        break;
+                }
+            }
+            return input;
+        }
+
+    }
+}
